7ff389cebdca5c258c600e14f514ee651927e927
codigix <codigixinfotech@gmail.com>
2025-08-19 15:21:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,87 +7,93 @@ import "./css/style.css";
 import Feedbackform from "./jsx/pages/Feedbackform";
 import { get } from "./services/apiServices";
 import { useDispatch, useSelector } from "react-redux";
-import { login, setPermissions } from "./store/actions/authActions";
+import { setPermissions } from "./store/actions/authActions";
 import OTPVerify from "./jsx/pages/OTPVerify";
 import ForgotPassword from "./jsx/pages/ForgotPassword";
 import VerifyForgetPasswordOTP from "./jsx/pages/VerifyForgetPasswordOTP";
 
-
-const Login = lazy(() => {
-	return new Promise((resolve) => {
+const Login = lazy(() =>
+	new Promise((resolve) => {
 		setTimeout(() => resolve(import("./jsx/pages/Login")), 500);
-	});
-});
+	})
+);
 
 function App() {
 	const navigate = useNavigate();
-
 	const { pathname } = useLocation();
+	const dispatch = useDispatch();
+	const { token } = useSelector((state) => state.auth);
 
-  const dispatch = useDispatch();
-  const { token } = useSelector(state => state.auth)
-
+	// Auto login check
 	useEffect(() => {
-		if (!pathname.includes("feedback-form") && !pathname.includes("verify-otp")) {
+		if (
+			!pathname.includes("feedback-form") &&
+			!pathname.includes("verify-otp")
+		) {
 			checkAutoLogin(navigate);
 		}
-	}, []);
+	}, [pathname, navigate]);
 
+	// Fetch permissions if logged in
 	useEffect(() => {
 		const fetchPermissions = async () => {
 			try {
 				const response = await get(`/role/test-permission`);
-         console.log( "permissions",response.data.permissions)
+				console.log("permissions", response.data.permissions);
 				dispatch(setPermissions(response?.data?.permissions));
 			} catch (error) {
-				console.log(error);
+				console.error("Error fetching permissions:", error);
 			}
 		};
-		
-    // if (token) {
-    //   fetchPermissions();
-    // }
-    
-	// }, []);
+
+		if (token) {
+			fetchPermissions();
+		}
+	}, [token, dispatch]);
 
 	const routeblog = (
 		<Routes>
 			<Route path="/login" element={<Login />} />
 			<Route path="/verify-otp" element={<OTPVerify />} />
 			<Route path="/forgot-password" element={<ForgotPassword />} />
-			<Route path="/verify-forget-password-otp" element={<VerifyForgetPasswordOTP />} />
+			<Route
+				path="/verify-forget-password-otp"
+				element={<VerifyForgetPasswordOTP />}
+			/>
 			<Route path="/feedback-form" element={<Feedbackform />} />
 		</Routes>
 	);
+
+	const toastConfig = {
+		position: "bottom-right",
+		autoClose: 5000,
+		hideProgressBar: false,
+		newestOnTop: false,
+		closeOnClick: true,
+		rtl: false,
+		pauseOnFocusLoss: true,
+		draggable: true,
+		pauseOnHover: true,
+		theme: "colored",
+		closeButton: (
+			<button
+				style={{
+					width: "30px",
+					backgroundColor: "inherit",
+					border: "none",
+					color: "white",
+				}}
+			>
+				X
+			</button>
+		),
+	};
 
 	if (localStorage.getItem("token")) {
 		return (
 			<>
 				<Index />
-				<ToastContainer
-					position="bottom-right"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-					theme="colored"
-					closeButton={
-						<button
-							style={{
-								width: "30px",
-								backgroundColor: "inherit",
-								border: "none",
-								color: "white",
-							}}
-						>
-							X
-						</button>
-					}
-				/>
+				<ToastContainer {...toastConfig} />
 			</>
 		);
 	} else {
@@ -105,34 +111,11 @@ function App() {
 					}
 				>
 					{routeblog}
-					<ToastContainer
-						position="bottom-right"
-						autoClose={5000}
-						hideProgressBar={false}
-						newestOnTop={false}
-						closeOnClick
-						rtl={false}
-						pauseOnFocusLoss
-						draggable
-						pauseOnHover
-						theme="colored"
-						closeButton={
-							<button
-								style={{
-									width: "30px",
-									backgroundColor: "inherit",
-									border: "none",
-									color: "white",
-								}}
-							>
-								X
-							</button>
-						}
-					/>
+					<ToastContainer {...toastConfig} />
 				</Suspense>
 			</div>
 		);
 	}
 }
-}
+
 export default App;
